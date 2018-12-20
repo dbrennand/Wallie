b2e8abaeaa70061e28f8bcc74f675496247b3279
Dextroz <25749668+Dextroz@users.noreply.github.com>
2018-12-20 11:26:12 +0000

    Present images now part of utils

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 try:
     import subprocess, click
-except ImportError:
-    print("Failed to import required modules for utils.py")
+    from huepy import yellow
+except ImportError as err:
+    print(f"Failed to import required modules: {err}")
 
 
 def download_image(image_url, file_name):
@@ -9,3 +10,13 @@ def download_image(image_url, file_name):
     subprocess.run(["wget", "-O", f"{file_name}.jpg", f"{image_url}"])
     click.secho("Download Complete!", fg="bright_yellow")
     return f"{file_name}.jpg"
+
+
+def present_images(images):
+    """Present image choices to the user and request choice."""
+    for num, item in enumerate(images, 0):
+        click.secho(
+            f"""Image: {num} -- {item[0]["author_name"]}\nProfile: {item[0]["author_profile"]}\nImage Link: {item[0]["full_image"]}\n""", fg="bright_yellow")
+    user_choice = int(input(yellow("Select your preferred image: ")))
+    user_choice = images[user_choice]
+    return user_choice
